1ace4f9baf6a9812dbdbb363168b26d69094fb33
andranikkk <ayvazyanandranik46@gmail.com>
2025-04-08 13:43:18 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/interview-ekosistema_alpha_2/",
+  base: "/",
   plugins: [react()],
   server: {
     open: true,
